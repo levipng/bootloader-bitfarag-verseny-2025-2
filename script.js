0ef54b6c2231e70f87fd7bad4e2eb1dc7466d449
Zsombi1089 <116731984+Zsombi1089@users.noreply.github.com>
2025-10-13 17:47:16 +0200

    17:47;js

--- script.js
+++ script.js
@@ -1,35 +1,63 @@
-let matrix = Array(12);
-for (let i = 0; i < 12; i++) {
-    matrix[i] = Array(12);
-    for (let j = 0; j < 12; j++) {
-        matrix[i][j] = i * 12 + j;
+start();
+function start(){
+    let matrix = Array(12);
+    for (let i = 0; i < 12; i++) {
+        matrix[i] = Array(12);
+        for (let j = 0; j < 12; j++) {
+            matrix[i][j] = i * 12 + j;
+        }
     }
-}
-matrix[2][1] = '<img src="0.jpg">';
-matrix[2][3] = '<p>cig</p>';
+    const gombokDiv = document.getElementById('gombok');
+    let urhajoV = 0;
+    let meglevoV = 0;
+    let megnyomottgombok = 0;
+    gombbetoltes();
+};
 
-const gombokDiv = document.getElementById('gombok');
+test();
 
-for (let i = 0; i < 12; i++) {
-    for (let j = 0; j < 12; j++) {
-        const gomb = document.createElement('button');
+function test(){
+    matrix[2][1] = '<img src="0.jpg">';
+    matrix[2][3] = '<p>cig</p>';
+    matrix[4][4] = '0';
+};
+
+function gombbetoltes(){
+    for (let i = 0; i < 12; i++) {
+        for (let j = 0; j < 12; j++) {
+            const gomb = document.createElement('button');
+            
+            const contentDiv = document.createElement('div');
+            contentDiv.className = 'content';
+            contentDiv.innerHTML = matrix[i][j];
+            
+            gomb.appendChild(contentDiv);
+            
+
+            if (matrix[i][j]==0){
+                gomb.onclick = function() {
+                    this.classList.add('revealed');
+                };
+            } else {
+                gomb.onclick = function() {
+                    this.classList.add('revealed');
+                    urhajokiiro();
+                };
+            }
         
-        const contentDiv = document.createElement('div');
-        contentDiv.className = 'content';
-        contentDiv.innerHTML = matrix[i][j];
-        
-        gomb.appendChild(contentDiv);
-        
-        gomb.onclick = function() {
-            this.classList.add('revealed');
-        };
-        
-        gombokDiv.appendChild(gomb);
+            gombokDiv.appendChild(gomb);
+        }
     }
-}
+};
 
-let negyzetV = 5
+function gombnyomas(){
+    ++megnyomottgombok
 
-let meglevoV = ((negyzetV/28)*100);
-document.getElementById("meglevo").style.width = meglevoV + "%";
-document.getElementById("meglevo").innerText = negyzetV;
+};
+
+function urhajokiiro(){
+    ++urhajoV
+    meglevoV = ((urhajoV/28)*100);
+    document.getElementById("meglevo").innerText = urhajoV;
+    document.getElementById("meglevo").style.width = meglevoV + "%";
+};
