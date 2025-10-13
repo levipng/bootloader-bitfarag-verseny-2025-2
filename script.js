580ac81504c91bbc6b87c05e87a601e294214863
Zsombi1089 <116731984+Zsombi1089@users.noreply.github.com>
2025-10-13 17:58:47 +0200

    17:58;js

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const gombokDiv = document.getElementById('gombok');
 let urhajoV = 0;
 let meglevoV = 0;
 let megnyomottgombok = 0;
+let matrix
 
 start();
+
 function start(){
-    let matrix = Array(12);
+    matrix = Array(12);
     for (let i = 0; i < 12; i++) {
         matrix[i] = Array(12);
         for (let j = 0; j < 12; j++) {
